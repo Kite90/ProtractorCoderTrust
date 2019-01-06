f10318d73d5d743f602fcac9eae306bd752a58ac
kraducki <kajetan.raducki@unity.pl>
2019-01-07 00:27:39 +0100

    Przekazanie plików na GIT dla uczestników webinaru

--- tests/newPageDemo.js
+++ tests/newPageDemo.js
@@ -1,7 +1,24 @@
 var mainPage = require('../pages/mainPage.js');
 var newPage = require('../pages/newPage.js');
+var fs = require('fs');
+
+function writeScreenShot(data, filename) {
+    var stream = fs.createWriteStream(filename);
+
+    stream.write(new Buffer(data, 'base64'));
+    stream.end();
+}
+
+var i = 0;
 
 describe("Wejście na stronę Angulara, przejście na nowszą i sprawdzenie nawigacji", function() {
+
+  afterEach(function() {
+    browser.takeScreenshot().then(function (png) {
+      writeScreenShot(png, 'exception' + i + '.png');
+  });
+  i++;
+  });
 
   it("Otworz starą stronę WWW", function() {
      mainPage.openPage();
